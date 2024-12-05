16a6bf7cc13146c4e1c6810b54aa49ee919d971a
Gabriela <gabriela.priaro@gmail.com>
2024-12-05 10:42:53 +0000

    Add project files.

---
+++ ExercicioFixacaoConstruThisSobrecargaEncapsulamento/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercicioFixacaoConstruThisSobrecargaEncapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
